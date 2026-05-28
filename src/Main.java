c68c8afee419df0f1e237dee2a26b82db89c88eb
Oto <katsadzeoto@gmail.com>
2026-05-28 23:30:48 +0400

    refactor: replace inline legality check in game loop with isLegal

--- src/Main.java
+++ src/Main.java
@@ -119,25 +119,8 @@ public class Main {
                 }
 
                 String card = hand.get(chosen);
-                boolean ok = false;
-                String cardColor = color(card);
-                String upColor = color(upCard);
-                String cardRank = rank(card);
-                String upRank = rank(upCard);
 
-                if (card.startsWith("W")) {
-                    ok = true;
-                } else if (cardColor.equals(upColor)) {
-                    ok = true;
-                } else if (!calledColor.equals("") && cardColor.equals(calledColor)) {
-                    ok = true;
-                } else if (cardRank.equals(upRank) && !cardRank.equals("NUMBER")) {
-                    ok = true;
-                } else if (cardRank.equals("NUMBER") && upRank.equals("NUMBER") && number(card) == number(upCard)) {
-                    ok = true;
-                }
-
-                if (!ok) {
+                if (!isLegal(card, upCard, calledColor)) {
                     Display.illegalCard(name, card);
                     hand.add(draw());
                     next();
